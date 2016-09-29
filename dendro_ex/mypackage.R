15aa2d56c206c935dd86f56e1724afb9f0603c3f
tmeits <tmeits@gmail.com>
2016-09-29 16:06:08 +0700

    Create mypackage.R

---
+++ dendro_ex/mypackage.R
@@ -0,0 +1,5 @@
+#
+install.packages('dplRCon', dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages('dplR', dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages('detrendeR', dependencies=TRUE, repos='http://cran.rstudio.com/')
+
