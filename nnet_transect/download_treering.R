2634ff4d7e902ebd0de5606dd24bd058d34ac5e7
tmeits <tmeits@gmail.com>
2016-11-15 16:57:58 +0700

    new files

---
+++ nnet_transect/download_treering.R
@@ -0,0 +1,36 @@
+## download treering data from ITRDB's ftp servers
+
+version <- "v705"                       # data version on FTP server
+
+base_path_crn <-
+  "ftp://ftp.ncdc.noaa.gov/pub/data/paleo/treering/chronologies/"
+base_path_rwl <-
+  "ftp://ftp.ncdc.noaa.gov/pub/data/paleo/treering/measurements/"
+
+continents <- c("africa",
+                "asia",
+                "australia",
+                "canada",
+                "europe",
+                "mexico",
+                "southamerica",
+                "usa")
+
+zip_files_crn <- paste("itrdb", version, continents,
+                       "crn.zip", sep = "-")
+zip_files_rwl <- paste("itrdb", version, continents,
+                       "rwl.zip", sep = "-")
+
+# dir.create("C:/Users/IVA/Dropbox/24516/nnet_transect/data/crn")
+# dir.create("data/rwl")
+
+n <- length(continents)
+
+for (i in 1:n) {
+  download.file(paste(base_path_crn, zip_files_crn[i], sep = ""),
+                file.path("data", "crn", zip_files_crn[i]))
+  unzip(file.path("data", "crn", zip_files_crn[i]), exdir = "data/crn")
+  download.file(paste(base_path_rwl, zip_files_rwl[i], sep = ""),
+                file.path("data", "rwl", zip_files_rwl[i]))
+  unzip(file.path("data", "rwl", zip_files_rwl[i]), exdir = "data/rwl")
+}
